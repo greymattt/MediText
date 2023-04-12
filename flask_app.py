978ba7531d556b9e2b7c929e91163c9f9ee8cf98
Aravind <ara.als30@gmail.com>
2023-04-12 15:53:23 +0530

    Update flask

--- flask_app.py
+++ flask_app.py
@@ -5,20 +5,30 @@ language: python3
 author: Aravind
 """
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from twilio.twiml.messaging_response import MessagingResponse
 import openai
+import re
 
 
-openai.api_key = "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"  # supply your API key however you choose
+openai.api_key = ""  # supply your API key however you choose
 
 SECRET_KEY = 'xzAMtIhuCZsiNIw3NuxAVDCbsI9VDfyy'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+# Test route
+@app.route('/')
+def index():
+    return 'Web App with Python Flask! Reloaded !!'
+
+
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     # Body
+    # data = request.get_json()
+    # body = data['Body']
     body = request.values.get('Body', None)
 
     # Start our TwiML response
@@ -33,9 +43,12 @@ def incoming_sms():
     # Open-AI connection
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": body}])
     response = completion.choices[0].message.content
+    response = re.sub("As an AI language model, ", "", response)
     resp.message(response)
-    return str(resp)
+    xml_response = resp
+    print(xml_response)
+    return Response(str(xml_response), mimetype="application/xml")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9999)
+    app.run(debug=True, port=6000)
